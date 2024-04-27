906d3afe074081762d2db0777672d83eafd478e3
Thomas Stichlmair <thomas.stichlmair@xitaso.com>
2024-04-27 22:49:44 +0200

    removed reflector from pacstrap

--- 1_arch_install_script.sh
+++ 1_arch_install_script.sh
@@ -99,7 +99,7 @@ mount --mkdir ${EFI_PARTITION} /mnt/boot
 swapon ${SWAP_PARTITION}
 
 echo "Installing arch base system"
-pacstrap -K /mnt base base-devel git linux linux-firmware vim openssh reflector intel-ucode
+pacstrap -K /mnt base base-devel git linux linux-firmware vim openssh intel-ucode
 
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
